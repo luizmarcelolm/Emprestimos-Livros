5d195e15c46c5e80387c2f01c29a0b09b6451ae6
luiz <luizmarcelolm@hotmail.com>
2023-01-17 21:42:30 -0300

    Criação do banco de dados

---
+++ Emprestimos Livros/Controllers/EmprestimosModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Emprestimos_Livros.Controllers
+{
+    public class EmprestimosModel
+    {
+        public int Id { get; set; }
+        public string Recebedor { get; set; }
+        public string Fornecedor { get; set; }
+        public string LivroEmprestado { get; set; }
+        public DateTime DataEmprestimo { get; set; } = DateTime.Now;
+
+    }
+}
